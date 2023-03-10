76530e87ced24351b1707abda581a4b856ebf6ce
ethanjm3 <123283710+ethanjm3@users.noreply.github.com>
2023-03-10 14:39:06 -0600

    added nav and changed layout

--- app.vue
+++ app.vue
@@ -1,9 +1,19 @@
-<script setup>
+<script>
 
+ 
 </script>
 
 <template>
 <v-layout>
+
+<v-navigation-drawer permanent location="left" class="d-flex flex-column justify-space-evenly h-screen">
+  <v-btn>Nav Test</v-btn>
+  <v-btn>Nav Test</v-btn>
+  <v-btn>Nav Test</v-btn>
+  <v-btn>Nav Test</v-btn>
+  <v-btn>Nav Test</v-btn>
+  <v-btn>Nav Test</v-btn>
+</v-navigation-drawer>
 <v-app-bar></v-app-bar>
 <v-main>
 <NuxtPage/>
