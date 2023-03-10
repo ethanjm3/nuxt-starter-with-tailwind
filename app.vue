9d3b37e0d37c1a4a625f1024432f024e16b285d7
ethanjm3 <123283710+ethanjm3@users.noreply.github.com>
2023-03-10 09:10:31 -0600

    added supabase-js package and fetched sample data from supabase

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <script>
-  const keyss = process.env.SUPABASE_KEY;
-  console.log(keyss)
+import {createClient} from '@supabase/supabase-js'
+const url = process.env.SUPABASE_URL;
+const key = process.env.SUPABASE_KEY;
+const supabase = createClient(url,key)
+  
+let { data: test, error } = await supabase
+  .from('tease')
+  .select('id,testdata')
+console.log(test)
+
 </script>
 
 <template>
